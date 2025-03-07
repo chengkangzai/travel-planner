a8da5ce5b132599e17d0e36b15ff8e1d39f58365
chengkangzai <pycck@hotmail.com>
2025-03-08 00:50:57 +0800

    wip

--- database/migrations/2025_03_07_223752_add_team_id_to_locations_table.php
+++ database/migrations/2025_03_07_223752_add_team_id_to_locations_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('locations', function (Blueprint $table) {
-            $table->foreignIdFor(Team::class)->index()->constrained()->cascadeOnDelete();
+//            $table->foreignIdFor(Team::class)->index()->constrained()->cascadeOnDelete();
         });
     }
 };
