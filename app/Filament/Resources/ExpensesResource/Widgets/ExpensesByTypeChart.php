9ac73ba48695298363cb9f6d1d6c6db0a722e7c1
chengkangzai <pycck@hotmail.com>
2025-03-31 21:32:16 +0800

    color

--- app/Filament/Resources/ExpensesResource/Widgets/ExpensesByTypeChart.php
+++ app/Filament/Resources/ExpensesResource/Widgets/ExpensesByTypeChart.php
@@ -29,7 +29,19 @@ class ExpensesByTypeChart extends ChartWidget
 
         // Get all the colors for the chart
         $colors = collect(ExpensesType::cases())
-            ->map(fn(ExpensesType $type) => Color::rgb($type->getColor()))
+            ->map(function (ExpensesType $type) {
+                return match ($type->getColor()) {
+                    'gray' => '#808080',
+                    'purple' => '#8B5CF6',
+                    'success' => '#10B981',
+                    'danger' => '#EF4444',
+                    'info' => '#3B82F6',
+                    'warning' => '#F59E0B',
+                    'primary' => '#4B82F6',
+                    'indigo' => '#4B0082',
+                    default => '#6B7280',
+                };
+            })
             ->toArray();
 
         return [
