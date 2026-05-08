139cd467b20307564eda3ec2062b8858837480bf
Ching Cheng Kang <pycck@hotmail.com>
2026-05-08 11:25:55 +0800

    improve ux

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -2,14 +2,14 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use Str;
-use Filament\Actions\Action;
 use App\Filament\Resources\UserResource;
 use App\Models\User;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Str;
 
 class ListUsers extends ListRecords
 {
@@ -23,6 +23,7 @@ class ListUsers extends ListRecords
                 ->mutateDataUsing(function (array $data) {
                     $data['team_id'] = filament()->getTenant()->id;
                     $data['password'] = bcrypt(Str::random());
+
                     return $data;
                 })
                 ->schema([
@@ -36,7 +37,7 @@ class ListUsers extends ListRecords
                         ->email()
                         ->validationMessages([
                             'unique' => 'The member already have an account, invite them instead.',
-                        ])
+                        ]),
                 ]),
 
             Action::make('invite')
@@ -44,21 +45,29 @@ class ListUsers extends ListRecords
                 ->icon('heroicon-o-user-plus')
                 ->color('gray')
                 ->action(function (array $data) {
-                    $user = User::query()->findOrFail($data['user_id']);
+                    $user = User::query()
+                        ->withoutGlobalScopes()
+                        ->findOrFail($data['user_id']);
                     $user->teams()->attach(filament()->getTenant()->id);
                 })
                 ->schema([
                     Select::make('user_id')
                         ->label('User')
                         ->searchable()
-                        ->getSearchResultsUsing(fn(string $search): array => User::query()
-                            ->whereDoesntHave('teams', fn($query) => $query->where('team_id', filament()->getTenant()->id))
+                        ->preload()
+                        ->getSearchResultsUsing(fn (string $search): array => User::query()
+                            ->withoutGlobalScopes()
+                            ->whereDoesntHave('teams', fn ($query) => $query->where('team_id', filament()->getTenant()->id))
                             ->where('email', 'like', "%{$search}%")
                             ->limit(10)
                             ->pluck('name', 'id')
                             ->toArray()
                         )
-                ])
+                        ->getOptionLabelUsing(fn ($value): ?string => User::query()
+                            ->withoutGlobalScopes()
+                            ->find($value)?->name
+                        ),
+                ]),
         ];
     }
 }
